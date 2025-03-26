8671553b5c0d91db487d1989b6c366ff58849f42
MuriloDev1 <muriloantunesdasilva264@gmail.com>
2025-03-26 19:39:07 -0300

    organização de componentes - header, layout, sections

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { Content, Greeting, Title, Subtitle, Description } from "./PageTitle";
+import { Content, Greeting, Title, Subtitle, Description } from "../layout/PageTitle";
 
 const Hero = () => {
   useEffect(() => {
